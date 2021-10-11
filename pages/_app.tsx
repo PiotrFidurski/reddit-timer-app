0aa71b4d4e72c1556fc111060c8e48dd06659a64
Chimson <crunor55@hotmail.com>
2021-10-11 17:35:36 +0200

    add GlobalStyle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import GlobalStyle from '@styled/GlobalStyle';
 import type { AppProps } from 'next/app';
 import { Normalize } from 'styled-normalize';
 
@@ -5,6 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Normalize />
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
---
+++ styled/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
