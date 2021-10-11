fccb34160083b8e896eaaef17d0deef33484a40a
Chimson <crunor55@hotmail.com>
2021-10-11 16:49:13 +0200

    add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
+  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>'],
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/$1',
+    '@styles/(.*)': '<rootDir>/styles/$1',
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['./**/*.{js,jsx,ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+};
