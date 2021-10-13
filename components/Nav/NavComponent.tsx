c473ed5970085f1a9fc9529d80bd3c40e2cf4724
Chimson <crunor55@hotmail.com>
2021-10-13 20:18:50 +0200

    NavComponent uses div tag instead of header tag

--- components/Nav/NavComponent.tsx
+++ components/Nav/NavComponent.tsx
@@ -5,20 +5,22 @@ import * as S from './styles';
 function NavComponent() {
   return (
     <div>
-      <S.Nav>
-        <Image width="40" height="40" src="/assets/images/reddit.png" quality={100} />
-        <S.Ul>
-          <S.Li>
-            <S.A href="/search">Search</S.A>
-          </S.Li>
-          <S.Li>
-            <S.A href="#about">About</S.A>
-          </S.Li>
-          <S.Li>
-            <S.A href="#how-it-works">How it works</S.A>
-          </S.Li>
-        </S.Ul>
-      </S.Nav>
+      <div>
+        <S.Nav>
+          <Image width="40" height="40" src="/assets/images/reddit.png" quality={100} />
+          <S.Ul>
+            <S.Li>
+              <S.A href="/search">Search</S.A>
+            </S.Li>
+            <S.Li>
+              <S.A href="#about">About</S.A>
+            </S.Li>
+            <S.Li>
+              <S.A href="#how-it-works">How it works</S.A>
+            </S.Li>
+          </S.Ul>
+        </S.Nav>
+      </div>
     </div>
   );
 }
