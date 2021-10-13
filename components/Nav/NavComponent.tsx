bf429a5254c5d92f60c9df2d52fdc61c1dd95df6
Chimson <crunor55@hotmail.com>
2021-10-13 20:25:33 +0200

    replace div tags with header

--- components/Nav/NavComponent.tsx
+++ components/Nav/NavComponent.tsx
@@ -4,24 +4,22 @@ import * as S from './styles';
 
 function NavComponent() {
   return (
-    <div>
-      <div>
-        <S.Nav>
-          <Image width="40" height="40" src="/assets/images/reddit.png" quality={100} />
-          <S.Ul>
-            <S.Li>
-              <S.A href="/search">Search</S.A>
-            </S.Li>
-            <S.Li>
-              <S.A href="#about">About</S.A>
-            </S.Li>
-            <S.Li>
-              <S.A href="#how-it-works">How it works</S.A>
-            </S.Li>
-          </S.Ul>
-        </S.Nav>
-      </div>
-    </div>
+    <S.Header>
+      <S.Nav>
+        <Image width="40" height="40" src="/assets/images/reddit.png" quality={100} />
+        <S.Ul>
+          <S.Li>
+            <S.A href="/search">Search</S.A>
+          </S.Li>
+          <S.Li>
+            <S.A href="#about">About</S.A>
+          </S.Li>
+          <S.Li>
+            <S.A href="#how-it-works">How it works</S.A>
+          </S.Li>
+        </S.Ul>
+      </S.Nav>
+    </S.Header>
   );
 }
 
