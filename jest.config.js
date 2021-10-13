68da5d9fc55f587f0473dec0022979e8bc2c4470
chimson <crunor55@hotmail.com>
2021-10-13 21:08:13 +0200

    Failing ci pipeline (#7)
    * NavComponent uses div tag instead of header tag
    
    * NavComponent uses div tag instead of header tag
    
    * replace div tags with header
    
    * replace header tag by div
    
    * replace div by header tag
    
    * replace .js cypress files with .ts
    
    * add ignore cypress tests to testPathIgnorePatterns jest.config.js option
    
    * add eslint-cypress plugin
    
    * edit main.yml file

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/coverage',
+    '<rootDir>/dist',
+    '<rootDir>/cypress',
+  ],
   moduleDirectories: ['<rootDir>/node_modules', '<rootDir>'],
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/$1',
