bcf103be9fc3f881acecb6c68c4e36bf37e54287
chimson <crunor55@hotmail.com>
2021-10-20 13:07:59 +0200

    Homepage styles (#10)
    * add responsive grid layout
    
    * fix NavComponent tests
    
    * fix Header tests
    
    * adjust styling for NavComponent
    
    * fix linting issues in next-env.d.ts

--- styled/HomePage.styled.ts
+++ styled/HomePage.styled.ts
@@ -2,6 +2,27 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   position: relative;
+  display: grid;
+  grid-gap: 1rem;
+  margin: 0 auto;
+  max-width: 1440px;
+  grid-template-columns: repeat(4, minmax(auto, 1fr));
+  grid-template-areas:
+    'navbar navbar navbar navbar'
+    'image image image image'
+    'buttons buttons buttons buttons'
+    'about-section about-section about-section about-section'
+    'how-it-works-section how-it-works-section how-it-works-section how-it-works-section'
+    'footer footer footer footer';
+
+  /* mobile breakpoint for reference later, 480px */
+  @media (min-width: 768px) {
+    grid-template-areas:
+      'navbar navbar navbar navbar'
+      'buttons buttons image image'
+      'about-section about-section how-it-works-section how-it-works-section'
+      'footer footer footer footer';
+  }
 `;
 
 export const Backdrop = styled.div`
@@ -11,18 +32,23 @@ export const Backdrop = styled.div`
   transform: skewY(-10deg);
   transform-origin: 0 50%;
   right: 0;
-  padding: clamp(220px, 10vw, 400px);
+  padding: clamp(220px, 15vw, 400px);
   background: linear-gradient(140deg, #252e7c, #040b3e 90%);
 `;
 
-export const IntroSection = styled.section`
-  padding: 3rem 0;
-  position: relative;
+export const ButtonsWrapper = styled.section`
+  grid-area: buttons;
+  max-width: 600px;
+  padding: 1rem;
+  align-items: center;
   display: flex;
-  justify-content: space-between;
-  max-width: 900px;
-  align-items: flex-start;
+  justify-content: space-evenly;
+  position: relative;
   margin: 0 auto;
+  width: 100%;
+  @media (min-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const Button = styled.button`
@@ -32,15 +58,38 @@ export const Button = styled.button`
   border-radius: 9999px;
   border: 2px solid #41434d;
   min-width: 135px;
+  max-height: 50px;
   cursor: pointer;
+  @media (min-width: 768px) {
+    min-width: 300px;
+  }
 `;
 
 export const ImageWrapper = styled.div`
-  max-width: 100px;
+  grid-area: image;
+  padding: 1rem;
   position: relative;
+  margin: 0 auto;
+  max-width: 550px;
   width: 100%;
-  height: 170px;
-  display: block;
-  justify-content: center;
-  align-items: center;
+  @media (min-width: 768px) {
+    max-width: auto;
+  }
+`;
+
+export const AboutWrapper = styled.section`
+  position: relative;
+  padding: 1rem;
+  grid-area: about-section;
+`;
+
+export const HowItWorksWrapper = styled.section`
+  padding: 1rem;
+  position: relative;
+  grid-area: how-it-works-section;
+`;
+
+export const Footer = styled.footer`
+  position: relative;
+  grid-area: footer;
 `;
