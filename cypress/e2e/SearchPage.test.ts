8fc394e5f8d5635b6be2eaf8fbc3f779442ac214
Chimson <crunor55@hotmail.com>
2021-10-25 14:20:28 +0200

    add fixtures option to heatmap test in searchpage

--- cypress/e2e/SearchPage.test.ts
+++ cypress/e2e/SearchPage.test.ts
@@ -8,10 +8,14 @@ describe('SearchPage tests', () => {
   });
 
   it('displays heatmap of subreddit posts when api call is successful', () => {
-    cy.intercept('http://localhost:3000/api/posts?subreddit=javascript').as('getPosts');
+    cy.intercept('http://localhost:3000/api/posts?subreddit=javascript', { fixture: 'pushshiftapi-response.json' }).as(
+      'getPosts'
+    );
+
     cy.visit('/');
 
     cy.findByText(/^Search$/).click();
+
     cy.findByRole('textbox', { name: /subreddit/i })
       .clear()
       .type('javascript');
