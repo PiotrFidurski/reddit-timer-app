45749c121bec21fb0397b5cda7ecf829c566017d
Chimson <crunor55@hotmail.com>
2021-10-25 13:06:02 +0200

    add SearchPage tests and heatmap tests

---
+++ cypress/e2e/SearchPage.test.ts
@@ -0,0 +1,62 @@
+describe('SearchPage tests', () => {
+  it('has a search input that can be typed into', () => {
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('javascript');
+  });
+
+  it('displays heatmap of subreddit posts when api call is successful', () => {
+    cy.intercept('http://localhost:3000/api/posts?subreddit=javascript').as('getPosts');
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('javascript');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.findByTestId(/heatmap/i);
+
+    cy.should('exist');
+  });
+
+  it('displays error message when api call fails', () => {
+    cy.intercept('http://localhost:3000/api/posts?subreddit=asdasdasd').as('getPosts');
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('asdasdasd');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.findByText(/loading/i);
+
+    cy.findByText(/Cant find data for this subreddit/i);
+
+    cy.should('exist');
+  });
+
+  it('search button cannot be clicked while request is in flight', () => {
+    cy.intercept('http://localhost:3000/api/posts?subreddit=javascript', { delay: 3000 }).as('getPosts');
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('javascript');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.findByText(/loading/i);
+
+    cy.findByText(/^Search$/).should('have.attr', 'disabled');
+  });
+});
+
+// eslint-disable-next-line jest/no-export
+export {};
---
+++ cypress/e2e/heatmap.test.ts
@@ -0,0 +1,81 @@
+import response from '../fixtures/pushshiftapi-response.json';
+
+describe('Heatmap tests', () => {
+  it('returns a 400 status code on bad request', () => {
+    cy.intercept(
+      'http://localhost:3000/api/posts?subreddit=asdasdasd',
+      (req) => delete req.headers['if-none-match']
+    ).as('getPosts');
+    cy.visit('/search');
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('asdasdasd');
+    cy.findByText(/^Search$/).click();
+
+    cy.wait('@getPosts').its('response.statusCode').should('equal', 400);
+  });
+
+  it('returns a 200 status code on success', () => {
+    cy.intercept('http://localhost:3000/api/posts?subreddit=webdev', (req) => delete req.headers['if-none-match']).as(
+      'getPosts'
+    );
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('webdev');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.wait('@getPosts').its('response.statusCode').should('equal', 200);
+  });
+
+  it('returns heatmap data in correct shape', () => {
+    cy.intercept('http://localhost:3000/api/posts?subreddit=reactjs', { fixture: 'pushshiftapi-response.json' }).as(
+      'getPosts'
+    );
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('reactjs');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.wait('@getPosts').its('response.body').should('deep.equal', response);
+  });
+
+  it('redditpost has all the properties required', () => {
+    const post = {
+      data: [
+        {
+          author: 'dulajkavinda',
+          created_utc: 1614335585,
+          id: 'lsv023',
+          score: 820,
+          subreddit: 'reactjs',
+          title:
+            'Built an app to solve the media bias in our country towards politicians. you can view every side of the story just by sliding a toggle!',
+        },
+      ],
+    };
+    cy.intercept('http://localhost:3000/api/posts?subreddit=reactjs', { fixture: 'redditpost-response.json' }).as(
+      'getPosts'
+    );
+
+    cy.visit('/search');
+
+    cy.findByRole('textbox', { name: /subreddit/i })
+      .clear()
+      .type('reactjs');
+
+    cy.findByText(/^Search$/).click();
+
+    cy.wait('@getPosts').its('response.body').should('deep.equal', post);
+  });
+});
+
+// eslint-disable-next-line jest/no-export
+export {};
