2ce78e5602af98c64a86d44ecf97656fc62f47eb
Chimson <crunor55@hotmail.com>
2021-10-25 15:25:34 +0200

    improved test naming to better resemble what it does

--- __tests__/utils/heatmap.spec.ts
+++ __tests__/utils/heatmap.spec.ts
@@ -1,6 +1,6 @@
 import { initialState } from '@utils/heatmap';
 
-test('"initialState" returns correct two dimensional array', () => {
+test('"initialState" returns correct array that resembles weekdays and hours', () => {
   const result = initialState();
 
   expect(result).toEqual(new Array(7).fill([]).map(() => new Array(24).fill([])));
