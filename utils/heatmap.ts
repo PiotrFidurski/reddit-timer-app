fd9b0d5c0fce179b9d0f5c5f54a0854314078b50
Chimson <crunor55@hotmail.com>
2021-10-27 20:39:52 +0200

    edit hours array to show every even hour

--- utils/heatmap.ts
+++ utils/heatmap.ts
@@ -13,30 +13,18 @@ function getDayAndHour(post: RedditPost) {
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const hours = [
-  '1:00am',
+  '12:00pm',
   '2:00am',
-  '3:00am',
   '4:00am',
-  '5:00am',
   '6:00am',
-  '7:00am',
   '8:00am',
-  '9:00am',
   '10:00am',
-  '11:00am',
   '12:00am',
-  '13:00pm',
   '14:00pm',
-  '15:00pm',
   '16:00pm',
-  '17:00pm',
   '18:00pm',
-  '19:00pm',
   '20:00pm',
-  '21:00pm',
   '22:00pm',
-  '23:00pm',
-  '24:00pm',
 ];
 
 const colorPallette = [
