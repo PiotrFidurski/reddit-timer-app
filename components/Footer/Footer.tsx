987351502073628aad3df667b337ba303034d365
Chimson <crunor55@hotmail.com>
2021-10-27 17:42:29 +0200

    add footer styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,15 +1,37 @@
 import Image from 'next/image';
 import * as React from 'react';
-import { StyledFooter } from './styles';
+import { A, Credits, Hr, Li, Links, LogoWrapper, Paragraph, StyledFooter, Ul } from './styles';
 
 function Footer() {
   return (
     <StyledFooter>
-      <a href="https://github.com/chimson/reddit-timer-app" target="_blank" rel="noreferrer">
-        Github
-      </a>
-      <Image width="40" height="40" src="/assets/images/reddit.png" layout="fixed" quality={100} alt="logo" />
-      <p>Copyright 2021, Piotr Fidurski</p>
+      <LogoWrapper>
+        <Image width="40" height="40" src="/assets/images/reddit.png" layout="fixed" quality={100} alt="logo" />
+        <Hr />
+      </LogoWrapper>
+      <Links>
+        <Ul>
+          <Li>
+            <A href="https://github.com/chimson/reddit-timer-app" target="_blank" rel="noreferrer">
+              Github
+            </A>
+          </Li>
+          <Li>
+            <A href="https://lukaszadam.com/illustrations" target="_blank" rel="noreferrer">
+              Illustrations by lukaszadam.com
+            </A>
+          </Li>
+          <Li>
+            <A href="https://profy.dev" target="_blank" rel="noreferrer">
+              Inspired by
+            </A>
+          </Li>
+        </Ul>
+        <Hr />
+      </Links>
+      <Credits>
+        <Paragraph>Copyright 2021, Piotr Fidurski</Paragraph>
+      </Credits>
     </StyledFooter>
   );
 }
--- components/Footer/styles.ts
+++ components/Footer/styles.ts
@@ -1,11 +1,75 @@
+import { LineOnHover } from '@styled/style-utils';
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
   position: relative;
-  padding: 0 1rem;
+  padding: 1rem 1rem 0 1rem;
+  background: ${(props) => props.theme.background.secondary};
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
   grid-area: footer;
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(auto, 1fr));
+  grid-template-areas:
+    'logo logo logo'
+    'links links links'
+    'credits credits credits';
   justify-content: space-between;
   align-items: center;
   min-height: 100px;
+  @media (min-width: 768px) {
+    grid-template-areas: 'logo links credits';
+    border-radius: 0;
+  }
+`;
+
+export const Links = styled.section`
+  grid-area: links;
+  font-size: 0.7rem;
+  width: auto;
+`;
+
+export const LogoWrapper = styled.section`
+  grid-area: logo;
+`;
+
+export const Credits = styled.section`
+  grid-area: credits;
+  color: ${(props) => props.theme.color.secondary};
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+export const Hr = styled.hr`
+  border: none;
+  background-color: ${(props) => props.theme.color.highlight};
+  height: 1px;
+  width: 100%;
+  margin: 0.7rem 0;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Ul = styled.ul`
+  list-style: none;
+  font-size: clamp(0.8rem, 1vw, 1rem);
+  padding: 0;
+`;
+
+export const Li = styled.li`
+  padding: 0.6rem 0;
+  width: min-content;
+`;
+
+export const A = styled.a`
+  ${LineOnHover}
+  transition: color 0.25s ease;
+  position: relative;
+`;
+
+export const Paragraph = styled.p`
+  font-size: clamp(0.8rem, 1vw, 1rem);
 `;
