d126b044926a2a6ecf2b9c3134cfe14f71de7b8d
Chimson <crunor55@hotmail.com>
2021-10-27 20:40:09 +0200

    make heatmap responsive

--- components/Heatmap/styles.ts
+++ components/Heatmap/styles.ts
@@ -3,13 +3,55 @@ import styled from 'styled-components';
 export const Section = styled.section`
   max-width: 1000px;
   grid-area: heatmap;
+  display: flex;
   margin: 0 auto;
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
+export const HeatmapWrapper = styled.div`
+  display: flex;
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
+
+export const TimeWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+  justify-content: space-between;
+  @media (min-width: 768px) {
+    max-width: 890px;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin-left: 70px;
+  }
+`;
+
+export const Time = styled.div`
+  background: ${(props) => props.theme.background.button};
+  max-width: 80px;
+  width: 100%;
+  font-size: 0.8rem;
+  min-height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 768px) {
+    min-height: 40px;
+  }
 `;
 
 export const DayWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   max-width: 960px;
   align-items: center;
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const Day = styled.div`
@@ -17,6 +59,9 @@ export const Day = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
+  min-height: 40px;
+  background: ${(props) => props.theme.background.secondary};
 `;
 
 export const Square = styled.div<{ bg: string }>`
