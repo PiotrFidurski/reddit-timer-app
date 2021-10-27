2d5bca626328973af0a8e830f88c241cb0507ef7
Chimson <crunor55@hotmail.com>
2021-10-27 17:41:35 +0200

    add back button test to SearchPage tests

--- __tests__/pages/SearchPage.test.tsx
+++ __tests__/pages/SearchPage.test.tsx
@@ -1,6 +1,7 @@
 import SearchPage from '@pages/search';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { render } from '@utils/test-utils';
+import { useRouter } from 'next/router';
 
 test('it renders an input and a label with name subreddit', () => {
   render(<SearchPage />);
@@ -12,6 +13,27 @@ test('it renders an input and a label with name subreddit', () => {
   expect(input).toBeInTheDocument();
 
   expect(label).toBeInTheDocument();
+});
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
+test('it has a go back button that links back to "/"', () => {
+  const back = jest.fn();
+
+  (useRouter as jest.MockedFunction<jest.Mock>).mockImplementationOnce(() => ({
+    asPath: '/search',
+    back,
+  }));
+
+  render(<SearchPage />);
+
+  const goBackButton = screen.getByRole('button', { name: /back/i });
+
+  fireEvent.click(goBackButton);
+
+  expect(back).toHaveBeenCalledTimes(1);
 });
 
 // eslint-disable-next-line jest/no-export
