80b359f8b98c52ff3c68e6b4c2b0b8ee9517ac55
Chimson <crunor55@hotmail.com>
2021-10-28 18:25:57 +0200

    add Menu component for smaller device sizes

---
+++ components/CollapsedMenu/CollapsedMenu.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import * as React from 'react';
+import Close from '../../public/assets/svg/close.svg';
+import Hamburger from '../../public/assets/svg/hamburger.svg';
+import { A, Li, Menu, Nav, ToggleMenuButton, Ul } from './style';
+
+function CollapsedMenu() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Nav>
+      {!open ? (
+        <ToggleMenuButton onClick={handleOpen}>
+          <Hamburger />
+        </ToggleMenuButton>
+      ) : null}
+      <Menu open={open}>
+        <ToggleMenuButton onClick={handleClose}>
+          <Close />
+        </ToggleMenuButton>
+        <Ul>
+          <Li>
+            <Link href="/search" passHref>
+              <A>Search</A>
+            </Link>
+          </Li>
+          <Li>
+            <A href="/#about">About</A>
+          </Li>
+          <Li>
+            <A href="/#how-it-works">How it works</A>
+          </Li>
+        </Ul>
+      </Menu>
+    </Nav>
+  );
+}
+
+export default CollapsedMenu;
