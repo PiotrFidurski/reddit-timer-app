67cdfafaa7897c0cd1de7a7b2182b352df2ff675
Chimson <crunor55@hotmail.com>
2021-10-29 12:31:23 +0200

    remove duplicate tests from NavComponent

--- components/Header/NavComponent.test.tsx
+++ components/Header/NavComponent.test.tsx
@@ -1,33 +1,18 @@
-import Home from '@pages/index';
 import { screen } from '@testing-library/react';
 import { render } from '@utils/test-utils';
-import NavComponent from './NavComponent';
+import Home from '../../pages/index';
 
-test('it has "About", "How it works", "Search" links', () => {
-  render(<NavComponent />);
-  const about = screen.getAllByText(/about/i);
-  const howItWorks = screen.getAllByText(/how it works/i);
-  const search = screen.getAllByText(/search/i);
-
-  about.map((el) => expect(el).toBeInTheDocument());
-  howItWorks.map((el) => expect(el).toBeInTheDocument());
-  search.map((el) => expect(el).toBeInTheDocument());
-});
-
-test('clicking on "about" and "how-it-works" link scrolls to correct sections', () => {
+test('it has "About", "How it works", "Search" links that link to correct sections', () => {
   render(<Home />);
 
-  const aboutLink = screen.getByRole('link', { name: /about/i });
+  screen.getAllByRole('link', { name: /^Search$/, hidden: true });
 
-  const howItWorksLink = screen.getByRole('link', { name: /how it works/i });
+  expect(screen.getAllByRole('link', { name: /^About$/, hidden: true })[0]).toHaveAttribute('href', '/#about');
+  expect(screen.getAllByRole('link', { name: /^How it works$/, hidden: true })[0]).toHaveAttribute(
+    'href',
+    '/#how-it-works'
+  );
 
-  const about = screen.getByRole('region', { name: /^about-section$/ });
-
-  const howItWorks = screen.getByRole('region', { name: /^how-it-works-section$/ });
-
-  expect(aboutLink).toHaveAttribute('href', '/#about');
-  expect(howItWorksLink).toHaveAttribute('href', '/#how-it-works');
-
-  expect(about).toHaveAttribute('id', 'about');
-  expect(howItWorks).toHaveAttribute('id', 'how-it-works');
+  expect(screen.getByRole('region', { name: /^about-section$/ })).toHaveAttribute('id', 'about');
+  expect(screen.getByRole('region', { name: /^how-it-works-section$/ })).toHaveAttribute('id', 'how-it-works');
 });
