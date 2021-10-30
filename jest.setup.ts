7f5f3617c1d05fd3d87ad1d6486079c53e2a294e
Chimson <crunor55@hotmail.com>
2021-10-30 18:37:57 +0200

    add msw server for api mocking

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
+import 'next';
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen());
+// reset any request handlers that are declared as a part of our tests
+// (i.e. for testing one-time error scenarios)
+afterEach(() => server.resetHandlers());
+// clean up once the tests are done
+afterAll(() => server.close());
