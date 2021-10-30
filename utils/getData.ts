d0ba783fef04f871361367922d115acefb30751f
Chimson <crunor55@hotmail.com>
2021-10-30 21:34:51 +0200

    use absolute paths in fetch

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,10 +1,10 @@
 import { RedditPost } from '@pages/api/posts';
 
-// const url =
-//   process.env.NODE_ENV === 'production' ? 'https://reddit-timer-app.vercel.app/api' : 'http://localhost:3000/api';
+const url =
+  process.env.NODE_ENV === 'production' ? 'https://reddit-timer-app.vercel.app/api' : 'http://localhost:3000/api';
 
 export async function getData(subreddit: string): Promise<{ data: Array<RedditPost> }> {
-  const response = await fetch(`api/posts?subreddit=${subreddit}`);
+  const response = await fetch(`${url}/posts?subreddit=${subreddit}`);
 
   const result: { data: Array<RedditPost> } = await response.json();
 
