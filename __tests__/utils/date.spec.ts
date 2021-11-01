a751474501858c810e30dd1f690dd22613e2d827
Chimson <crunor55@hotmail.com>
2021-11-01 19:45:36 +0100

    add date var

--- __tests__/utils/date.spec.ts
+++ __tests__/utils/date.spec.ts
@@ -1,10 +1,10 @@
 import { getDateInEpoch as mockGetDateInEpoch } from '@utils/date';
 
-const FIXED_SYSTEM_TIME = 1635793200000;
+const date = new Date('2021-11-01Z20:00:00');
 
 beforeEach(() => {
   jest.useFakeTimers('modern');
-  jest.setSystemTime(FIXED_SYSTEM_TIME);
+  jest.setSystemTime(date);
 });
 
 afterEach(() => {
@@ -12,15 +12,13 @@ afterEach(() => {
 });
 
 jest.mock('@utils/date', () => ({
-  getDateInEpoch: jest.fn(() => ['1619895600', '1627844400', '1604257200']),
+  getDateInEpoch: jest.fn(() => ['1619895600', '1627844400', '1604260800']),
 }));
 
 test('it returns array of dates in epoch time', () => {
-  console.log(new Date(FIXED_SYSTEM_TIME), new Date('2021-11-01Z19:00:00'));
-
-  const sixMonthsAgo = (new Date(FIXED_SYSTEM_TIME).setMonth(new Date().getMonth() - 6) / 1000).toFixed();
-  const threeMonthsAgo = (new Date(FIXED_SYSTEM_TIME).setMonth(new Date().getMonth() - 3) / 1000).toFixed();
-  const yearAgo = (new Date(FIXED_SYSTEM_TIME).setFullYear(new Date().getFullYear() - 1) / 1000).toFixed();
+  const sixMonthsAgo = (new Date(date).setMonth(new Date().getMonth() - 6) / 1000).toFixed();
+  const threeMonthsAgo = (new Date(date).setMonth(new Date().getMonth() - 3) / 1000).toFixed();
+  const yearAgo = (new Date(date).setFullYear(new Date().getFullYear() - 1) / 1000).toFixed();
   const result = mockGetDateInEpoch();
   expect(result).toEqual([sixMonthsAgo, threeMonthsAgo, yearAgo]);
 });
