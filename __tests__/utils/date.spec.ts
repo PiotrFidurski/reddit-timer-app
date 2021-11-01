cf0870a92228598c4c48e7bb1dacbe9190e8b18f
Chimson <crunor55@hotmail.com>
2021-11-01 17:01:06 +0100

    add useFakeTimers call to date.spec.ts

--- __tests__/utils/date.spec.ts
+++ __tests__/utils/date.spec.ts
@@ -1,11 +1,13 @@
 import { getDateInEpoch as mockGetDateInEpoch } from '@utils/date';
 
 jest.mock('@utils/date', () => ({
-  getDateInEpoch: jest.fn(() => ['1619862245', '1627811045', '1604227445']),
+  getDateInEpoch: jest.fn(() => ['1594422000', '1602370800', '1578700800']),
 }));
 
+jest.useFakeTimers().setSystemTime(new Date('2021-01-11').getTime());
+
 test('it returns array of dates in epoch time', () => {
-  jest.spyOn(Date, 'now').mockImplementation(() => 1635763444799);
+  jest.spyOn(Date, 'now').mockImplementation(() => 1610323200000);
 
   const sixMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 6) / 1000).toFixed();
   const threeMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 3) / 1000).toFixed();
