e635773daf8ba46b7bb8fa58636ae58515d6677b
Chimson <crunor55@hotmail.com>
2021-11-01 17:46:21 +0100

    add timezone to jest config

--- jest.setup.ts
+++ jest.setup.ts
@@ -9,3 +9,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 // clean up once the tests are done
 afterAll(() => server.close());
+
+export default () => {
+  process.env.TZ = 'CST';
+};
