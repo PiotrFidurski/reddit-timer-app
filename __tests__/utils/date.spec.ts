3ba7db0448e001e7ff52191a4e14dfda118424ea
Chimson <crunor55@hotmail.com>
2021-11-01 11:45:47 +0100

    fix date in date.spec.ts to pass locall

--- __tests__/utils/date.spec.ts
+++ __tests__/utils/date.spec.ts
@@ -1,16 +1,14 @@
 import { getDateInEpoch as mockGetDateInEpoch } from '@utils/date';
 
 jest.mock('@utils/date', () => ({
-  getDateInEpoch: jest.fn(() => ['1619180447', '1627042847', '1603455647']),
+  getDateInEpoch: jest.fn(() => ['1619862245', '1627811045', '1604227445']),
 }));
 
 test('it returns array of dates in epoch time', () => {
-  jest.spyOn(Date, 'now').mockImplementation(() => 1634991646894);
+  jest.spyOn(Date, 'now').mockImplementation(() => 1635763444799);
 
   const sixMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 6) / 1000).toFixed();
-
   const threeMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 3) / 1000).toFixed();
-
   const yearAgo = (new Date(Date.now()).setFullYear(new Date().getFullYear() - 1) / 1000).toFixed();
 
   const result = mockGetDateInEpoch();
