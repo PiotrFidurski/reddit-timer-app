d32c486145747de6e26c614068ffd4b4b9cb8cb3
Chimson <crunor55@hotmail.com>
2021-11-01 12:09:09 +0100

    fix search input shortening during initial loading state

--- components/Form/styles.ts
+++ components/Form/styles.ts
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  max-width: 450px;
   grid-area: form;
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 1rem;
