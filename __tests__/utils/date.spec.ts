b8997e0bfbf697148c6f18cfb2725a7e9e861c28
chimson <crunor55@hotmail.com>
2021-11-02 14:45:32 +0100

    add console logs to compare dates in CI and locally (#25)
    * use setUTC and getUTC date methods
    * change order of getDateInEpoch array

--- __tests__/utils/date.spec.ts
+++ __tests__/utils/date.spec.ts
@@ -1,8 +1,10 @@
-const FIXED_SYSTEM_TIME = 1635790347000;
+import { getDateInEpoch as mockGetDateInEpoch } from '@utils/date';
+
+const date = new Date(Date.UTC(2021, 11, 10, 13, 0, 0));
 
 beforeEach(() => {
   jest.useFakeTimers('modern');
-  jest.setSystemTime(FIXED_SYSTEM_TIME);
+  jest.setSystemTime(date);
 });
 
 afterEach(() => {
@@ -10,15 +12,16 @@ afterEach(() => {
 });
 
 jest.mock('@utils/date', () => ({
-  getDateInEpoch: jest.fn(() => ['1619889147', '1627837947', '1604254347']),
+  getDateInEpoch: jest.fn(() => ['1631278800', '1623330000', '1607605200']),
 }));
 
 test('it returns array of dates in epoch time', () => {
-  // const sixMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 6) / 1000).toFixed();
-  // const threeMonthsAgo = (new Date(Date.now()).setMonth(new Date().getMonth() - 3) / 1000).toFixed();
-  // const yearAgo = (new Date(Date.now()).setFullYear(new Date().getFullYear() - 1) / 1000).toFixed();
-  // const result = mockGetDateInEpoch();
-  // expect(result).toEqual([sixMonthsAgo, threeMonthsAgo, yearAgo]);
+  const sixMonthsAgo = (new Date(date).setUTCMonth(new Date(date).getUTCMonth() - 6) / 1000).toFixed();
+  const threeMonthsAgo = (new Date(date).setUTCMonth(new Date(date).getUTCMonth() - 3) / 1000).toFixed();
+  const yearAgo = (new Date(date).setUTCFullYear(new Date(date).getUTCFullYear() - 1) / 1000).toFixed();
+
+  const result = mockGetDateInEpoch();
+  expect(result).toEqual([threeMonthsAgo, sixMonthsAgo, yearAgo]);
 });
 
 // eslint-disable-next-line jest/no-export
