fad3c02a3dd4afb17165a271892329d43a6a4702
Chimson <crunor55@hotmail.com>
2021-11-03 12:37:13 +0100

    add 1 more submission to mocked data array

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -11,12 +11,20 @@ export const handlers = [
         data: [
           {
             author: 'dulajkavinda',
-            created_utc: 1614335585,
+            created_utc: 1638658800,
             id: 'lsv023',
             score: 820,
             subreddit: 'reactjs',
             title:
               'Built an app to solve the media bias in our country towards politicians. you can view every side of the story just by sliding a toggle!',
+          },
+          {
+            author: 'dulajkavinda',
+            created_utc: 1614870054,
+            id: 'la62if',
+            score: 600,
+            subreddit: 'reactjs',
+            title: 'React Exercises With Detailed Solutions',
           },
         ],
       })
