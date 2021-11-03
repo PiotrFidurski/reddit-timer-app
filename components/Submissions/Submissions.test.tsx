a726cc050c3f9385d73a1853e20047e1e098b5f4
Chimson <crunor55@hotmail.com>
2021-11-03 16:50:05 +0100

    modify stub response in handlers to fit utc date methods, revert submission test to previous state

--- components/Submissions/Submissions.test.tsx
+++ components/Submissions/Submissions.test.tsx
@@ -1,52 +1,36 @@
-import { screen } from '@testing-library/react';
-import { initialState } from '@utils/heatmap';
+import SearchPage from '@pages/search';
+import { fireEvent, screen } from '@testing-library/react';
 import { render } from '@utils/test-utils';
-import Submissions from './Submissions';
 
-const heatMap = initialState();
+test('it displays correct submissions for each square', async () => {
+  render(<SearchPage />);
 
-heatMap[0][0].push({
-  author: 'dulajkavinda',
-  created_utc: 1638658800,
-  id: 'lsv023',
-  score: 820,
-  subreddit: 'reactjs',
-  num_comments: 175,
-  full_link: 'https://www.reddit.com/r/reactjs/comments/lsv023/built_an_app_to_solve_the_media_bias_in_our/',
-  title:
-    'Built an app to solve the media bias in our country towards politicians. you can view every side of the story just by sliding a toggle!',
-});
+  await screen.findByTestId(/heatmap/i);
 
-heatMap[4][16].push({
-  author: 'mikasarei',
-  created_utc: 1614870054,
-  full_link: 'https://www.reddit.com/r/reactjs/comments/lxmeow/react_exercises_with_detailed_solutions/',
-  id: 'lxmeow',
-  num_comments: 56,
-  score: 724,
-  subreddit: 'reactjs',
-  title: 'React Exercises With Detailed Solutions',
-});
+  const monday2am = screen.getByTestId(/^0_day-Monday$/);
+  const friday12pm = screen.getByTestId(/^23_day-Friday$/);
 
-test('it displays correct submissions for each square', () => {
-  render(<Submissions data={heatMap} time={{ day: 0, hour: 0 }} />);
+  fireEvent.click(monday2am);
 
-  expect(screen.getByText(/Built an app to solve the media bias in our country/i)).toBeInTheDocument();
+  expect(screen.getByText(/Mothers Day CSS Card/i)).toBeInTheDocument();
 
-  render(<Submissions data={heatMap} time={{ day: 4, hour: 16 }} />);
+  fireEvent.click(friday12pm);
 
-  expect(screen.getByText(/React Exercises With Detailed Solutions/i)).toBeInTheDocument();
+  expect(screen.getByText(/The Amazing Mechanical Plumbus/i)).toBeInTheDocument();
 });
 
 test('it has a link to each submission', async () => {
-  render(<Submissions data={heatMap} time={{ day: 0, hour: 0 }} />);
+  render(<SearchPage />);
+
+  await screen.findByTestId(/heatmap/i);
+
+  const monday2am = screen.getByTestId(/^0_day-Monday$/);
+
+  fireEvent.click(monday2am);
 
   const link = screen.getByRole('link', { name: /submission-link/ });
 
   expect(link).toHaveAttribute('target', '_blank');
 
-  expect(link).toHaveAttribute(
-    'href',
-    'https://www.reddit.com/r/reactjs/comments/lsv023/built_an_app_to_solve_the_media_bias_in_our/'
-  );
+  expect(link).toHaveAttribute('href', 'https://www.reddit.com/r/javascript/comments/n8r1fy/mothers_day_css_card/');
 });
