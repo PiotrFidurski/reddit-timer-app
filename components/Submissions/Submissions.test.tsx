7713be72519c7a534879afe4699c77a8e2365a3e
Chimson <crunor55@hotmail.com>
2021-11-03 16:12:04 +0100

    modify submission tests to accept dummy data instead of making real api call

--- components/Submissions/Submissions.test.tsx
+++ components/Submissions/Submissions.test.tsx
@@ -1,32 +1,45 @@
-import SearchPage from '@pages/search';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { initialState } from '@utils/heatmap';
 import { render } from '@utils/test-utils';
+import Submissions from './Submissions';
 
-test('it displays correct submissions for each square', async () => {
-  render(<SearchPage />);
+const heatMap = initialState();
 
-  await screen.findByTestId(/heatmap/i);
+heatMap[0][0].push({
+  author: 'dulajkavinda',
+  created_utc: 1638658800,
+  id: 'lsv023',
+  score: 820,
+  subreddit: 'reactjs',
+  num_comments: 175,
+  full_link: 'https://www.reddit.com/r/reactjs/comments/lsv023/built_an_app_to_solve_the_media_bias_in_our/',
+  title:
+    'Built an app to solve the media bias in our country towards politicians. you can view every side of the story just by sliding a toggle!',
+});
 
-  const sunday12pm = screen.getByTestId(/^0_day-Sunday$/);
-  const thursday4pm = screen.getByTestId(/^16_day-Thursday$/);
+heatMap[4][16].push({
+  author: 'mikasarei',
+  created_utc: 1614870054,
+  full_link: 'https://www.reddit.com/r/reactjs/comments/lxmeow/react_exercises_with_detailed_solutions/',
+  id: 'lxmeow',
+  num_comments: 56,
+  score: 724,
+  subreddit: 'reactjs',
+  title: 'React Exercises With Detailed Solutions',
+});
 
-  fireEvent.click(sunday12pm);
+test('it displays correct submissions for each square', () => {
+  render(<Submissions data={heatMap} time={{ day: 0, hour: 0 }} />);
 
   expect(screen.getByText(/Built an app to solve the media bias in our country/i)).toBeInTheDocument();
 
-  fireEvent.click(thursday4pm);
+  render(<Submissions data={heatMap} time={{ day: 4, hour: 16 }} />);
 
   expect(screen.getByText(/React Exercises With Detailed Solutions/i)).toBeInTheDocument();
 });
 
 test('it has a link to each submission', async () => {
-  render(<SearchPage />);
-
-  await screen.findByTestId(/heatmap/i);
-
-  const sunday12pm = screen.getByTestId(/^0_day-Sunday$/);
-
-  fireEvent.click(sunday12pm);
+  render(<Submissions data={heatMap} time={{ day: 0, hour: 0 }} />);
 
   const link = screen.getByRole('link', { name: /submission-link/ });
 
