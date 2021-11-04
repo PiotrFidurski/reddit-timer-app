85fd7afa55641e5349368e543e625c6ade25d6ba
Chimson <crunor55@hotmail.com>
2021-11-04 16:39:54 +0100

    add e2e sort functionality tests

--- cypress/e2e/ui/SearchPage.test.ts
+++ cypress/e2e/ui/SearchPage.test.ts
@@ -73,6 +73,48 @@ describe('SearchPage tests', () => {
       href: 'https://www.reddit.com/r/reactjs/comments/o01jqv/trying_to_access_text_of_a_div/',
     });
   });
+
+  it('sorts posts by ascending and descending order', () => {
+    cy.interceptAndSearch({ subreddit: 'reactjs', delay: 2000 });
+
+    cy.wait('@getPosts');
+
+    cy.findByTestId(/^13_day-Wednesday$/).click();
+
+    cy.findByRole('button', { name: /sort-order/i }).as('orderBtn');
+
+    cy.get('@orderBtn').click();
+
+    cy.findAllByRole('article')
+      .eq(0)
+      .should('contain.text', 'React Pivot Table component that connects to Azure Analysis Services');
+
+    cy.get('@orderBtn').click();
+
+    cy.findAllByRole('article').eq(0).should('contain.text', 'Storybook now supports Vite and Webpack5');
+  });
+
+  it('sorts posts by specified type', () => {
+    cy.interceptAndSearch({ subreddit: 'reactjs', delay: 2000 });
+
+    cy.wait('@getPosts');
+
+    cy.findByTestId(/^13_day-Wednesday$/).click();
+
+    cy.findByRole('button', { name: /sort-by-date/i }).click();
+
+    cy.findAllByRole('article').eq(0).should('contain.text', 'Stand aside Unity. Move over unreal engine.');
+
+    cy.findByRole('button', { name: /sort-by-comments/i }).click();
+
+    cy.findAllByRole('article')
+      .eq(0)
+      .should('contain.text', 'Been learning React the past few months and built a Netflix Clone');
+
+    cy.findByRole('button', { name: /sort-by-score/i }).click();
+
+    cy.findAllByRole('article').eq(0).should('contain.text', 'Storybook now supports Vite and Webpack5');
+  });
 });
 
 // eslint-disable-next-line jest/no-export
