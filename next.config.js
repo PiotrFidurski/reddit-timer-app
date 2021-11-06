af0e0b77f1a60d3711b4b6b019187e26147ab60a
Chimson <crunor55@hotmail.com>
2021-11-06 16:01:49 +0100

    add BASE_URL client side env variable

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.NODE_ENV === 'production' ? 'https://reddit-timer-app.vercel.app' : 'http://localhost:3000',
+  },
 };
